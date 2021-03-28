65d6bc2fd751c643762972ce440dd002b54bd59f
José Airton <airtonsmorais28@gmail.com>
2021-03-28 20:20:39 -0300

    Initial commit

---
+++ src/main/java/br/com/airton/ContadordePessoas.java
@@ -0,0 +1,55 @@
+package br.com.airton;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ViewScoped
+@ManagedBean(name = "contador")
+public class ContadordePessoas {
+	
+	private int limitePessoas; // limite de pessoas no local
+	
+	private int quantPresente; // quantidade de pessoas presentes no local
+	
+	private int quantPessoas; // contador de pessoas que passaram no restaurante
+
+	public int getLimitePessoas() {
+		return limitePessoas;
+	}
+
+
+	public void setLimitePessoas(int limitePessoas) {
+		this.limitePessoas = limitePessoas;
+	}
+
+
+	public int getQuantPresente() {
+		return quantPresente;
+	}
+
+
+	public void setQuantPresente(int quantPresente) {
+		this.quantPresente = quantPresente;
+	}
+
+
+	public int getQuantPessoas() {
+		return quantPessoas;
+	}
+
+
+	public void setQuantPessoas(int quantPessoas) {
+		this.quantPessoas = quantPessoas;
+	}
+	
+	
+
+	
+	public void entrada() {		
+		quantPresente += getQuantPresente();
+	}
+	
+	
+	
+
+}
